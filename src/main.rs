7c2894e50c5319b90e919e2d4a67f3955dd97f80
usummon <breakcreak@gmail.com>
2019-08-24 17:39:26 -0600

    Added an error message for NotFound errors

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sfml::graphics::{RenderWindow, Color, RenderTarget, Text, Font};
 use sfml::window::{Style, Event, Key};
 
 use std::process::Command;
+use std::io::{self, Write, ErrorKind};
 
 fn main() {
     //create terminal window
@@ -13,14 +14,16 @@ fn main() {
         Style::CLOSE,
         &Default::default()
     );
+    //-----
     let mut run_command = false;
+    //define text
     let mut command = String::from(">>");
     let font = Font::from_file("resources/sansation.ttf").unwrap();
-    //define text
     let mut text = Text::new(&command, &font, 12);
     text.set_fill_color(&Color::MAGENTA);
     text.set_outline_color(&Color::BLACK);
     text.set_outline_thickness(5.0);
+    //-----
     //loop till terminal is closed
     while terminal.is_open() {
         let event = terminal.poll_event();
@@ -56,6 +59,7 @@ fn main() {
         if run_command {
             output = command_run(&command);
             run_command = false;
+            text.set_string(&output);
         }
 
         terminal.draw(&text);
@@ -65,8 +69,25 @@ fn main() {
 
 fn command_run(input: &String) -> String {
     let command = input[2..input.len()].to_string();
+    println!("should print before error");
+    let child = Command::new(command).output();
+    println!("should not print");
 
-    let child = Command::new(command).spawn();
-
+    match child.is_err() {
+        true => {
+            
+            let message = error_message(child.unwrap_err().kind());
+            println!("ERROR: {}", message);
+        },
+        false => io::stdout().write_all(&child.unwrap().stdout).unwrap(),
+    }
+    
     String::from("ok")
+}
+
+fn error_message(kind: ErrorKind) -> String {
+    match kind {
+        ErrorKind::NotFound => return String::from("The command or entity searched for was not found"),
+        _ => return String::from("An unexpected error has occured..."),
+    }
 }
